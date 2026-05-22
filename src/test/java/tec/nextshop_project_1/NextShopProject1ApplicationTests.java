575a92d763194ca588a43b7feb813acc2e8d1503
Edgardo <edgardomora2010@gmail.com>
2026-05-21 19:22:01 -0600

    Commit Inicial - NextShop Project 1

---
+++ src/test/java/tec/nextshop_project_1/NextShopProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tec.nextshop_project_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextShopProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
